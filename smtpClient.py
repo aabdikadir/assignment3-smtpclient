0a13e9a55bea863aad5b3521092c229ecf175379
aabdikadir <124479410+aabdikadir@users.noreply.github.com>
2023-03-04 16:47:01 -0600

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -8,7 +8,7 @@
 from socket import *
 
 
-def smtp_client(port=1025, mailserver='127.0.0.1'):
+def smtp_client(port=25, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
 
